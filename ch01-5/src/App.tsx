340a671ff26d6827e1b013ea9aae62c2dba79567
goodji <vptl7211@naver.com>
2025-04-10 23:32:31 +0900

    [Feat]: fake data 넣기

--- ch01-5/src/App.tsx
+++ ch01-5/src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as D from './data'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          {D.randomName()}, {D.randomJobTitle()}, {D.randomDayMonthYear()}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+        <img src = {D.randomAvatar()} height="50" />
+        <img src ={D.randomImage()} height="300"/>
 
-export default App;
+      </div>
+  )
+}
